d5f075d36e5034e39c96cb4fa37aae8310cde6c6
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2023-08-20 15:28:59 -0400

    everything designed and showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="content">
+        <div className="videos">
+          {/* <!-- a video starts --> */}
+          <div className="video">
+            <div className="thumbnail">
+              <img src="https://img.youtube.com/vi/tEmcg_aEbDE/maxresdefault.jpg" alt="" />
+            </div>
+
+            <div className="details">
+              <div className="author">
+                <img src="https://yt3.ggpht.com/ytc/AOPolaQijIdiATc56tpfUHVA5xCjv2Sk6yatn9G7obFEng=s68-c-k-c0x00ffffff-no-rj" alt="" />
+              </div>
+              <div className="title">
+                <h3>
+                  Pursuit Our Mission & Impact
+                </h3>
+                <a href="">
+                  Pursuit
+                </a>
+                <span> 502 Views • 1 Year Ago </span>
+              </div>
+            </div>
+          </div>
+          {/* <!-- a video Ends --> */}
+          {/* <!-- a video starts --> */}
+          <div className="video">
+            <div className="thumbnail">
+              <img src="https://img.youtube.com/vi/tEmcg_aEbDE/maxresdefault.jpg" alt="" />
+            </div>
+
+            <div className="details">
+              <div className="author">
+                <img src="https://yt3.ggpht.com/ytc/AOPolaQijIdiATc56tpfUHVA5xCjv2Sk6yatn9G7obFEng=s68-c-k-c0x00ffffff-no-rj" alt="" />
+              </div>
+              <div className="title">
+                <h3>
+                  Pursuit Our Mission & Impact
+                </h3>
+                <a href="">
+                  Pursuit
+                </a>
+                <span> 502 Views • 1 Year Ago </span>
+              </div>
+            </div>
+          </div>
+          {/* <!-- a video Ends --> */}
+          {/* <!-- a video starts --> */}
+          <div className="video">
+            <div className="thumbnail">
+              <img src="https://img.youtube.com/vi/tEmcg_aEbDE/maxresdefault.jpg" alt="" />
+            </div>
+
+            <div className="details">
+              <div className="author">
+                <img src="https://yt3.ggpht.com/ytc/AOPolaQijIdiATc56tpfUHVA5xCjv2Sk6yatn9G7obFEng=s68-c-k-c0x00ffffff-no-rj" alt="" />
+              </div>
+              <div className="title">
+                <h3>
+                  Pursuit Our Mission & Impact
+                </h3>
+                <a href="">
+                  Pursuit
+                </a>
+                <span> 502 Views • 1 Year Ago </span>
+              </div>
+            </div>
+          </div>
+          {/* <!-- a video Ends --> */}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
