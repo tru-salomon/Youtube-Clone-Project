b5a0669bcc466520667821b725860bcf15a6c144
christophersanchez <christophersanchez@pursuit.org>
2023-08-21 19:45:32 -0400

    Updated App.jsx to function properly. ErroPage now shows 🫡

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ErrorPage from './components/ErrorPage/ErrorPage';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const userInput = 'Examplee'; // Simulated misspelling
+
+  // Set a condition for displaying the ErrorPage component (when there's a misspelling)
+  const showErrorPage = userInput.includes('Examplee'); // Change this to your specific misspelled word
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {showErrorPage ? (
+        <ErrorPage /> // Render ErrorPage when showErrorPage is true
+      ) : (
+
+        <div>
+          {/* Content to display when showErrorPage is false */}
+        
+          <h1>Welcome to My App</h1>
+          <p>Type the correct word: {userInput}</p>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
