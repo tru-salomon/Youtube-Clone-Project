d646425358a822c484ef938bf77c143306050cdb
christophersanchez <christophersanchez@pursuit.org>
2023-08-21 11:24:38 -0400

    Basic 🩻 for ErrorPage + jsx + css ✨

---
+++ src/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './ErrorPage.module.css';
+
+const ErrorPage = () => {
+  return (
+    <div className={styles.errorPage}>
+      <h1>Oops! Something went wrong.</h1>
+      <p>We're sorry, but the page you're looking for doesn't exist.</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
