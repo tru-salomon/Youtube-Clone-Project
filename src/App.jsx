7ed7b854e3ca45ed8edac769792f852185ea8649
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2023-08-23 20:14:37 -0400

    route structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,25 @@
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import React from "react"
-import {useEffect} from "react"
-import './App.css'
-import NavBar from "./Components/NavBar.jsx"
-import Cards from "./Components/Cards.jsx"
-import Video from "./Components/Video.jsx"
-import ErrorPage from "./Components/ErrorPage.jsx"
-import SearchBar from "./Components/SearchBar.jsx"
-import About from "./Components/About.jsx"
-// import Card from "./Components/Card.jsx"
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Box} from '@material-ui/core';
 
 
 
-const YT_API_KEY = import.meta.env.YT_CLONE_API_KEY;
- const YT_API_ENDPOINT= `https://www.googleapis.com/youtube/v3/search`;
 
 function App() {
-
-useEffect(()=>{
-  fetch(`${YT_API_ENDPOINT}?part=snippet&maxResults=10&q=${""}&key=${YT_API_KEY}`)
-    
-   .then(response =>response.json())
-    .then(data => console.log(data))
-},[])
-
-
   return (
-
-  
-    <div className="App">
-      <Router>
-        <NavBar />
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "white" }}>
+        <Navbar/> 
         <Routes>
-          <Route path="/" element={<Cards />}></Route>
-          <Route path="*" element={<ErrorPage />}></Route>
-          <Route path="/About" element={<About/>}></Route>
-          <Route path="/videos/:videoID" element={<Video />}></Route>
-          <Route path="/search/:searchBar" element={<SearchBar />}></Route>
+          <Route path="/" excat element={<Home-Feed />} />
+          <Route path="/video/:id" element={<Video />} />
+          <Route path="/:channel/:id" element={<Channel />} />
+          <Route path="/:search/:searchQuery" element={<SearchResult />} />
+
         </Routes>
-      </Router>
-    <h1>You tube </h1>
-    
-    </div>
+
+        </Box>
+      
+    </BrowserRouter>
   );
 }
 
