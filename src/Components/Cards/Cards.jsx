f387de1ef410b2c3c5f6995884426f2be8e33995
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2023-08-24 03:12:50 -0400

    fixed cards appearances

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,29 +1,19 @@
-export default function Cards() {
+import Card from "../Card/Card";
+import "./cards.css";
 
+export default function Cards() {
     return (
         <>
-            {/* <!-- a video starts --> */}
-            < div className="video" >
-                <div className="thumbnail">
-                    <img src="https://img.youtube.com/vi/tEmcg_aEbDE/maxresdefault.jpg" alt="" />
+            <div className="content">
+                <div className="videos">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
                 </div>
-
-                <div className="details">
-                    <div className="author">
-                        <img src="https://yt3.ggpht.com/ytc/AOPolaQijIdiATc56tpfUHVA5xCjv2Sk6yatn9G7obFEng=s68-c-k-c0x00ffffff-no-rj" alt="" />
-                    </div>
-                    <div className="title">
-                        <h3>
-                            Pursuit Our Mission & Impact
-                        </h3>
-                        <a href="">
-                            Pursuit
-                        </a>
-                        <span> 502 Views • 1 Year Ago </span>
-                    </div>
-                </div>
-            </div >
-            {/* <!-- a video Ends --> */}
+            </div>
         </>
     )
 }
