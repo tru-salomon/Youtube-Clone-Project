25f8524f0a4df646fa1be187bc50432cbc941593
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2023-08-23 23:29:42 -0400

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import { Stack } from '@mui/material'
+import { Link } from 'react-router-dom'
+import { logo } from "../../utils/constants";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Paper, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Stack direction="row" alignItems="center" p={2} sx={{ position: "sticky", background: '#000', top: 0, justifyContent: 'space-between' }}>
+
+      <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+        <img src={logo} alt="logo" height="45px" />
+      </Link>
+
+      <Paper
+        component="form"
+        onSubmit={() => { }}
+        sx={{
+          borderRadius: 20,
+          border: '1px solid #e3e3e3',
+          pl: 2,
+          boxShadow: 'none',
+          mr: { sm: 5 }
+        }}
+      >
+        <input className='search-bar'
+          placeholder='Search...'
+          value=""
+          onChange={() => { }} />
+        <IconButton type='submit' sx={{ p: '10px', color: 'red' }}>
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+    </Stack>
   )
 }
 
