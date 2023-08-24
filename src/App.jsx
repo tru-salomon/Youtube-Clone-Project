d3dcf407d26f567deb3637371603be56175f2086
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2023-08-24 00:34:44 -0400

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 import { Navbar, Home_Feed, Video, Channel, SearchResult } from './components/';
-
+import './App.css';
 
 
 
--- src/components/Home-Feed/Home-Feed.jsx
+++ src/components/Home-Feed/Home-Feed.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { categories } from '../../utils/constants';
+
+const selectedCategory = 'New';
 
 const Home_Feed = () => {
   return (
-    <div>Home_Feed</div>
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+      <Box sx={{ height: { sx: "auto", md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
+        <Stack direction='row' sx={{ overflowY: "auto", height: { sx: 'auto', md: '95%' }, flexDirection: { md: 'column' } }}>
+          {categories.map((category) => (
+            <button key={category.name} className='sidebar-btn' style={{ background: category.name === selectedCategory && "red", width: '100%' }}>
+              <span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px' }}>{category.icon}</span>
+              <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8' }}>{category.name}</span>
+            </button>
+          ))}
+        </Stack>
+
+        <Typography className='copyright' variant='body2' sx={{ mt: 1.5 }}>
+          © 2023 Pursuit Fellows
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography variant="h4" fontWeight='bold' mb={2}>
+          New <span style={{ color: 'red' }}>Videos</span>
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
