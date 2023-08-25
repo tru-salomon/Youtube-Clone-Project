3b3fdc2afdae315ca95dff00a50265a8a650a77e
christophersanchez <christophersanchez@pursuit.org>
2023-08-25 10:49:05 -0400

    Fixed emoji issue in ErrorPage 🤪

--- src/Components/ErrorPopup/ErrorPage.jsx
+++ src/Components/ErrorPopup/ErrorPage.jsx
@@ -1,37 +1,37 @@
 import { BrowserRouter } from "react-router-dom";
 import "./ErrorPage.css";
 import { useNavigate } from "react-router-dom";
-import {useEffect} from "react"
+import { useEffect } from "react"
 
 export default function ErrorPage() {
-  useEffect(()=>{
-var body=document.body;
-var interval= setInterval(createStar,100)
+  useEffect(() => {
+    var body = document.body;
+    var interval = setInterval(createStar, 100)
 
-function createStar (){
-  var right = Math.random() * 500;
-  var top = Math.random() * screen.height;
-  var star = document.createElement("div")
-  star.classList.add("star")
-  body.appendChild(star)
-  var starInterval = setInterval(runStar,10)
-  star.style.top = top + "px"
+    function createStar() {
+      var right = Math.random() * 500;
+      var top = Math.random() * screen.height;
+      var star = document.createElement("div")
+      star.classList.add("star")
+      body.appendChild(star)
+      var starInterval = setInterval(runStar, 10)
+      star.style.top = top + "px"
 
-  function runStar(){
-    if (right >= screen.width){
-      star.remove();
-      clearInterval(starInterval)
+      function runStar() {
+        if (right >= screen.width) {
+          star.remove();
+          clearInterval(starInterval)
+        }
+        right += 3;
+        star.style.right = right + "px"
+      }
+
     }
-    right += 3;
-    star.style.right = right + "px"
-  }
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
 
-}
-return()=>{
-  clearInterval(interval)
-}
-  },[])
-  
   const navigate = useNavigate();
   function navigateToSafety() {
     navigate("/");
@@ -42,7 +42,7 @@ return()=>{
       <div className="text"></div>
       <h1> 404 </h1>
 
-      <h3>Oops! Something went wrong.dizzy_face:🤪 dizzy_face:</h3>
+      <h3>Oops! Something went wrong. 🤪 </h3>
       <p>We're sorry, but the page you're looking for doesn't exist.</p>
       <button onClick={navigateToSafety} className="btn btn-primary">
         Return to Home
