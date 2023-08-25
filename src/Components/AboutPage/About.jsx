be2f69ffd6c8e45aa0a3d91207b324a33422ddef
christophersanchez <christophersanchez@pursuit.org>
2023-08-24 23:24:09 -0400

    Updated AboutPage with Shaq & Ricon. Made dynamic profile name and fixed pics ✨

--- src/Components/AboutPage/About.jsx
+++ src/Components/AboutPage/About.jsx
@@ -11,20 +11,21 @@ export default function About() {
       bio:
         "Shaquala is a cat lover. She loves God and has 2 daughters who bring her joy. She strives for greatness and wants to pay it forward so that others may have opportunities to fulfill their dreams.",
       githubLink: "https://github.com/shaqfred",
-      image: "shaquala.jpeg"
+      image: "https://avatars.githubusercontent.com/u/96317236?s=96&v=4",
+
     },
     {
       name: "Ricon",
       bio:
         "I've grown passionate about promoting Open Source and Open Standards as I delve into writing code to enhance the workflow and productivity of everyday leaders. Currently, as I explore my potential for crafting technological content through my involvement in the Pursuit program as a 10.1 cohort fellow, I'm dedicated to honing my skills in software engineering. With a background that's as diverse as it is unconventional, my aim is to stretch limits and devise solutions that connect with folks.",
       githubLink: "https://github.com/RAWdevX21",
-      image: "ricon.jpeg"
+      image: "https://avatars.githubusercontent.com/u/80225161?v=4"
     },
     {
       name: "Chris",
       bio: "It's been uphill battle throughout this module for Chris. He is an animal aficiado with a passion for the healthcare of both people and birds. He wants to incorporate his prior knowledge into the software industry and has been making strides; both big and small. He worked hard on his ErrorPage to give a fun effect and likes messing around with CSS. ",
       githubLink: "https://github.com/CSAN7690",
-      image: "chris.jpeg"
+      image: "https://avatars.githubusercontent.com/u/127960419?v=4"
     },
 
     {
@@ -51,8 +52,9 @@ export default function About() {
 
       <div className="section">
         <h2>About the Developers</h2>
-        {developers.map((developer, index, image) => (
+        {developers.map((developer, index) => (
           <div className="developer" key={index}>
+            {developer.image && <img src={developer.image} alt={developer.name} className="developer-image" />}
             <h3 data-text={developer.name} className="retro-text">
               {developer.name}
             </h3> {/* Some fancy effects ✨ */}
